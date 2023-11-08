ade19375dc1ff0088f1b992ef55e890072b0e703
Koromonnnnnnnn <764363@dpsk12.net>
2023-11-08 09:41:18 -0700

    f

--- master.cpp
+++ master.cpp
@@ -4,9 +4,21 @@ using namespace std;
 
 bool gameLoop = true;
 
+// Shop
+int gold = 100;
+void shop();
+
 // Gens
 string monsterGen();
 string lootGen();
+
+// Inventory
+string inventory[10];
+
+// Return
+string monster;
+string loot;
+
 
 // Battle
 void initiatebossFight();
@@ -21,6 +33,8 @@ void damageSong();
 
 int main()
 {
+    for (int i = 0; i < 10; i++)
+        inventory[i] = " ";
 
     int room = 1;
     char direction;
@@ -35,40 +49,50 @@ int main()
         switch (room)
         {
         case 1:
-            cout << "you're in room 1, you can go (E)ast" << endl;
+            cout << "You are in 'Room One', do you wish to go (E)ast towards 'Room Two'?" << endl;
             cin >> direction;
-            if (direction == 'E')
+            if (direction == 'E' || direction == 'e')
                 room = 2;
             break;
         case 2:
-            cout << "you're in room 2, you can go (W)est or (S)outh" << endl;
+            cout << "You are in 'Room Two', do you wish to go (S)outh towards 'Room Three'? Or (W)est back to 'Room One'" << endl;
             cin >> direction;
-            if (direction == 'W')
+            if (direction == 'W' || direction == 'w')
                 room = 1;
-            if (direction == 'S')
+            if (direction == 'S' || direction == 's')
                 room = 3;
             break;
         case 3:
-            cout << "you're in room 3, you can go (N)orth or (S)outh" << endl;
+            cout << "You are in 'Room Three', you can go (S)outh to 'Room Four' (Cave), or (N)orth back to 'Room Two'" << endl;
             cin >> direction;
-            if (direction == 'N')
+            if (direction == 'N' || direction == 'n')
                 room = 2;
-            if (direction == 'S')
+            if (direction == 'S' || direction == 's')
                 room = 4;
             break;
         case 4:
-            cout << "you're in room 4, you can go (E)ast or (N)orth" << endl;
+            cout << "You are in 'Room Four', you can go (E)ast to 'Room Five', or (N)orth back to 'Room Three'" << endl;
             cin >> direction;
-            if (direction == 'N')
+            if (direction == 'N' || direction == 'n')
                 room = 3;
-            if (direction == 'E')
+            if (direction == 'E' || direction == 'e')
                 room = 5;
             break;
         case 5:
-            cout << "you're in room 5, you can go (W)est" << endl;
+            cout << "You are in 'Room Five', you can go (N)orth to 'Room Six', or (W)est back to 'Room Four'" << endl;
             cin >> direction;
-            if (direction == 'W')
+            if (direction == 'W' || direction == 'w')
                 room = 4;
+            if (direction == 'N' || direction == 'N')
+                room = 6;
+            break;
+        case 6:
+            cout << "You are in 'Room Six', you can go (E)ast to 'Room Seven', or (S)outh back to 'Room Five'" << endl;
+            cin >> direction;
+            if (direction == 'E' || direction == 'e')
+                room = 4; //fix when add more rooms (change to 7)
+            if (direction == 'S' || direction == 'e')
+                room = 5;
             break;
         }
     }
@@ -127,5 +151,60 @@ void victorySong()
     {
         Beep(notes[i], durations[i]);
         Sleep(100);
+    }
+}
+
+void shop()
+{
+    int input;
+    while (input != '0')
+    {
+        cout << "welcome to the shop" << endl;
+        cout << "items for sale:" << endl;
+        cout << "1) Cupcake: $5" << endl;
+        cout << "2) Sock: $10" << endl;
+        cout << "3) Key: $50" << endl;
+        cout << "Your Inventory:" << endl;
+        cout << "0 to QUIT" << endl;
+        cout << "Your Gold: " << gold << endl;
+        for (int i = 0; i < 10; i++)
+            cout << inventory[i] << " ";
+        cout << endl;
+        cout << "What would you like to purchase?" << endl;
+        cin >> input;
+        if (input == 1)
+        {
+            if (gold >= 5)
+            {
+                cout << "You bought the cupcake!" << endl;
+                inventory[1] = "Cupcake";
+                gold -= 5;
+            }
+            else
+                cout << "You dont have enough gold!" << endl;
+        }
+        else if (input == 2)
+        {
+            if (gold >= 10)
+            {
+                cout << "You bought the sock!" << endl;
+                inventory[2] = "Sock";
+                gold -= 10;
+            }
+        }
+        else if (input == 3)
+        {
+            if (gold > -50)
+            {
+                cout << "You bought the key!" << endl;
+                inventory[0] = "Key";
+                gold -= 50;
+            }
+        }
+        else
+        {
+            cout << "not an option, dummy" << endl;
+        }
+        break;
     }
 }
